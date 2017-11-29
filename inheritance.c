118afeeea1b6eb3381a7d22a52532f3e13366a4f
sariebeary <shb66@pitt.edu>
2017-11-29 01:04:06 -0500

    added components

--- inheritance.c
+++ inheritance.c
@@ -7,48 +7,68 @@
 #include <pthread.h>
 #include <sched.h>
 
-static pthread_t t1, t2, t3;
+static pthread_t t1, t2, t3; //declare threads
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; // mutex variable
 static pthread_mutexattr_t mta; // mutex attribute variable
-//static sched_param sp; // structure variable for buffering thread priority
+struct sched_param param; //declare structure variable for buffering thread priority
+int policy = SCHED_RR;
+int priority = 10;
 
 void function()
 {
  //do some work here.
-    pthread_mutex_lock(&mutex);
+    
 }
 
-void function1()
+void *thread1handler()
 {
  //function 1 for thread 1.
  //do some work here
+    pthread_exit(NULL);
 }
 
-void function2()
+void *thread2handler()
 {
 //function 2 for thread 2.
 //do some work here and call function()
+    pthread_mutex_lock(&mutex);
+    pthread_mutex_unlock(&mutex);
+    pthread_exit(NULL);
 }
 
-void function3()
+void *thread3handler()
 {
 //function 3 for thread 3.
 //do some work here
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
 {
-//create mutex and initialize it.
-    pthread_mutexattr_init (&mta);
+    //create mutex and initialize it.
+    pthread_mutexattr_init(&mta);
+    //set protocol for mutex attribute
     pthread_mutexattr_setprotocol(&mta, PTHREAD_PRIO_INHERIT);
+    //initialize mutex with attribute
     pthread_mutex_init(&mutex, &mta);
-//create thread 1, thread 2 and thread 3
-    pthread_create(&t1, NULL, (void *) &function1, NULL);
-    pthread_create(&t1, NULL, (void *) &function2, NULL);
-    pthread_create(&t1, NULL, (void *) &function3, NULL);
-
-//set different priority for each thread
-//set handler for thread 1, thread 2 and thread 3 respectively. For instance, function 1 for thread 1, function 2 for thread 2 and function 3 for thread 3
- return 0;
+    //create thread 1, thread 2 and thread 3
+    //set handler for thread 1, thread 2 and thread 3 respectively
+    pthread_create(&t1, NULL, (void *) thread1handler, NULL);
+    pthread_create(&t2, NULL, (void *) thread2handler, NULL);
+    pthread_create(&t3, NULL, (void *) thread3handler, NULL);
+    
+    param.sched_priority = priority;
+    
+    int maxpriority = sched_get_priority_max(policy);
+    int minpriority = sched_get_priority_min(policy);
+    //set different priority for each thread
+    pthread_setschedparam(t1, policy, &param);
+    //pthread_getschedparam(t1, &policy, &param);
+    
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_join(t3, NULL);
+    
+    return 0;
 }
 
